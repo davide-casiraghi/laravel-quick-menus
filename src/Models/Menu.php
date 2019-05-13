6cba63bd5b5a869f1902c9352488b5d4e8a300d9
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-13 18:52:32 +0200

    added controllers

---
+++ src/Models/Menu.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    /***************************************************************************/
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'menus';
+
+    /***************************************************************************/
+
+    protected $fillable = [
+        'name',
+    ];
+}
---
+++ src/Models/MenuItemTranslation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItemTranslation extends Model
+{
+    /***************************************************************************/
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'menu_item_translations';
+
+    /***************************************************************************/
+
+    public $timestamps = false;
+    protected $fillable = ['name', 'description', 'slug'];
+}
