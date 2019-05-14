ed9eb2d4cfce8b871cc16b32b0c637fda039a642
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-14 11:16:22 +0200

    testing all the menu controller except show

--- tests/MenuControllerTest.php
+++ tests/MenuControllerTest.php
@@ -30,16 +30,16 @@ class ContinentControllerTest extends TestCase
     }
 
     /** @test */
-    /*public function it_displays_the_menu_create_page()
+    public function it_displays_the_menu_create_page()
     {
         $this->authenticateAsAdmin();
         $this->get('menus/create')
             ->assertViewIs('laravel-quick-menus::menus.create')
             ->assertStatus(200);
-    }*/
+    }
 
     /** @test */
-    /*public function it_stores_a_valid_menu()
+    public function it_stores_a_valid_menu()
     {
         $this->authenticateAsAdmin();
 
@@ -49,17 +49,17 @@ class ContinentControllerTest extends TestCase
 
         //$this->assertDatabaseHas('menus', $attributes);
         $response->assertRedirect('/menus/');
-    }*/
+    }
 
     /** @test */
-    /*public function it_does_not_store_invalid_menu()
+    public function it_does_not_store_invalid_menu()
     {
         $this->authenticateAsAdmin();
 
         $response = $this->post('/menus', []);
         $response->assertSessionHasErrors();
         $this->assertNull(Menu::first());
-    }*/
+    }
 
     /** @test */
     /*public function it_displays_the_menu_show_page()
@@ -73,7 +73,7 @@ class ContinentControllerTest extends TestCase
     }*/
 
     /** @test */
-    /*public function it_displays_the_menu_edit_page()
+    public function it_displays_the_menu_edit_page()
     {
         $this->authenticateAsAdmin();
 
@@ -81,10 +81,10 @@ class ContinentControllerTest extends TestCase
         $response = $this->get("/menus/{$menu->id}/edit");
         $response->assertViewIs('laravel-quick-menus::menus.edit')
                  ->assertStatus(200);
-    }*/
+    }
 
     /** @test */
-    /*public function it_updates_valid_menu()
+    public function it_updates_valid_menu()
     {
         // https://www.neontsunami.com/posts/scaffolding-laravel-tests
         $this->authenticateAsAdmin();
@@ -95,10 +95,10 @@ class ContinentControllerTest extends TestCase
         $response = $this->put("/menus/{$menu->id}", $attributes);
         $response->assertRedirect('/menus/');
         $this->assertEquals('Updated', $menu->fresh()->name);
-    }*/
+    }
 
     /** @test */
-    /*public function it_does_not_update_invalid_menu()
+    public function it_does_not_update_invalid_menu()
     {
         $this->authenticateAsAdmin();
 
@@ -106,10 +106,10 @@ class ContinentControllerTest extends TestCase
         $response = $this->put("/menus/{$menu->id}", []);
         $response->assertSessionHasErrors();
         $this->assertEquals('Example', $menu->fresh()->name);
-    }*/
+    }
 
     /** @test */
-    /*public function it_deletes_menus()
+    public function it_deletes_menus()
     {
         $this->authenticateAsAdmin();
 
@@ -117,5 +117,5 @@ class ContinentControllerTest extends TestCase
         $response = $this->delete("/menus/{$menu->id}");
         $response->assertRedirect('/menus');
         $this->assertNull($menu->fresh());
-    }*/
+    }
 }
