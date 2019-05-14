ca3b73e28245cc4a2bbee52891924bc54423e087
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-14 09:53:06 +0200

    added database test migrations

---
+++ tests/database/migrations/2019_05_14_070525844132_create_menu_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('menu_id')->nullable();
+            $table->string('parent_item_id')->nullable();
+            $table->integer('type')->nullable();
+            $table->string('url')->nullable();
+            $table->string('route')->nullable();
+            $table->string('font_awesome_class')->nullable();
+            $table->boolean('hide_name')->default('0');
+            $table->string('compact_name');
+            $table->integer('order')->nullable();
+            $table->integer('access')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
